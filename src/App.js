3912f8976dbe7802c26ce8cb42d82c4fa8360cb9
usama <usamamuhammad231@gmail.com>
2020-08-25 19:53:43 +0500

    messege commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
-                    <br> updated by usama</br>
+                    <br /> updated by usama
                 </p>
                 <a
                     className="App-link"
